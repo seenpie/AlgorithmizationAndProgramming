2ecccc6a506e5f813fee56fbe47d14b83d423411
nikstarygin <nikita.starygin2017@yandex.ru>
2025-12-07 11:30:59 +0300

    solution for cos

---
+++ tests/Tasks.Tests/TrigonometricFunctionsCosTests.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using Tasks.TrigonometricFunctions;
+using Xunit;
+
+namespace Tasks.Tests
+{
+    public class TrigonometricFunctionsCosTests
+    {
+        public static IEnumerable<object[]> GetTestCases()
+        {
+            var solutions = new ITrigonometricFunctionsSolution[]
+            {
+                new Tasks.TrigonometricFunctions.TrigonometricFunctionsCos()
+            };
+
+            var testPoints = new ((double x, double e), double expected)[]
+            {
+                ((100, 0.0001), Math.Cos(100 * Math.PI / 180)),
+                ((71, 0.0001), Math.Cos(71 * Math.PI / 180)),
+                ((360, 0.0001), Math.Cos(360 * Math.PI / 180)),
+                ((22, 0.0001), Math.Cos(22 * Math.PI / 180)),
+                ((1111, 0.00001), Math.Cos(1111 * Math.PI / 180)),
+            };
+
+            foreach (var solution in solutions)
+            {
+                foreach (var ((x, e), expected) in testPoints)
+                {
+                    yield return new object[] { solution, x, e, expected };
+                }
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void CheckFunctionLogic(ITrigonometricFunctionsSolution solution, double x, double e, double expected)
+        {
+            // Act
+            double actual = solution.Calculate(x, e);
+
+            // Assert
+            Assert.Equal(expected, actual, 5);
+        }
+    }
+}
